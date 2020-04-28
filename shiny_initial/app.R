99dd1c0815385fb4ea905ffeadb8236c9ab93a38
Tate Huffman <thuffman@college.harvard.edu>
2020-04-28 01:36:53 -0400

    Done? Maybe?

--- shiny_initial/app.R
+++ shiny_initial/app.R
@@ -1,10 +1,15 @@
 library(shiny)
 library(tidyverse)
 
-# combines 
+# combines pitch RDS files that were created in other files
+# this is because Git LFS can only take files up to 2 GB in size
 
 pitches_app <- readRDS("pitches_early.rds") %>% 
   bind_rows(readRDS("pitches_late.rds"))
+
+# this code classifies players as either pitchers or hitters
+# creates a merged dataset with number of pitches as a pitcher or hitter
+# then classifies players by whichever of the two is greater
 
 pitchers <- pitches_app %>% 
   group_by(pitcher_name) %>% 
@@ -16,11 +21,16 @@ hitters <- pitches_app %>%
   summarize(count_b = n()) %>% 
   arrange(desc(count_b))
 
+# the full_join() means we don't lose any players by combining the two tibbles
+# if they don't have any pitches as a pitcher or hitter, they become zero
+
 merged <- full_join(hitters, pitchers, by = c("batter_name" = "pitcher_name")) %>% 
   mutate(count_p = replace_na(count_p, 0),
          count_b = replace_na(count_b, 0),
          player_type = ifelse(count_p > count_b, "pitcher", "batter")) %>% 
   rename(name = "batter_name")
+
+# reads in the prediction and accuracy RDS files also created elsewhere
 
 predictions_standard <- readRDS("model_preds_standard.rds")
 accuracy_standard <- readRDS("accuracy_standard.rds")
@@ -28,15 +38,18 @@ predictions_simplified <- readRDS("model_preds_simplified.rds")
 accuracy_simplified <- readRDS("accuracy_simplified.rds")
 
 
-ui <- navbarPage("Final Project",
+ui <- navbarPage("Pitch Sequencing",
+                 
+                 # homepage!
+                 # the images used in this page are found in graphics.Rmd
                  
                  tabPanel("Home",
-                          titlePanel("Effects of Pitch Sequencing on MLB Pitcher Success"),
+                          titlePanel("An Exploration of MLB Pitch Sequencing"),
                           mainPanel(
                             p("There's more data in baseball than ever now. Whether you're interested 
                               in the probability a Mike Trout line drive will fall for a hit, the top 
                               speed Ronald Acuña Jr. reaches going from first to third, or the precise 
-                              arm slot at which Josh Hader releases each fastball, these measurements 
+                              arm slot at which Gerrit Cole releases each fastball, these measurements 
                               are all available at quite literally the push of a button. Yet as the 
                               game becomes viewed by many as a series of sequences and estimations, 
                               there remain questions about what hasn't yet been quantified."
@@ -70,7 +83,7 @@ ui <- navbarPage("Final Project",
                             img(src = "pctpitches.png", height = 400, width = 600),
                             p("It's clear sliders, while always popular, have seen their usage skyrocket in
                               recent seasons, while the past two years have seen the two-seam fastball drop
-                              from the second-most used pitch to the fourth, trailing the changeup and the
+                              from the second-most used pitch to the fourth, now both trailing the changeup and
                               curveball. Sinkers, a fastball variant often grouped together with the
                               two-seamer, have also decreased in frequency, with these two together close to
                               being overtaken by the cut fastball. Now let's revisit our initial sequence
@@ -82,23 +95,22 @@ ui <- navbarPage("Final Project",
                               portfolio in comparison to starters, relying more heavily on sequences
                               involving four-seam fastballs and sliders and less heavily on those with
                               two-seamers and changeups. This makes sense intuitively, given that reliever
-                              outings are typically on the order of one to two innings and thus would be
-                              expected to rely more heavily on pitch quality to defeat opposing hitters and
-                              less on pitch mix and variation over the course of an extended appearance."),
+                              outings are typically on the order of one to two innings, so they're expected
+                              to rely more heavily on pitch quality to defeat opposing hitters and less
+                              on pitch mix and variation over the course of an extended appearance."),
                             p("That's my broad introduction to pitch sequencing - look at the rest of
                               this site to learn more!")
                             )
-                            ),
+                          ),
                  
-                 # this tab allows the user to see the five most common sequences by player
-                 # this can be most common thrown by a pitcher or most common seen by a hitter
-                 # we also include dynamic selection to adjust year based on min. pitches thrown/faced
-                 
+                 # this tab allows the user to see the most common sequences by player
+                 # also gives information about common pitches that might pop up
+
                  tabPanel("Explore",
                           titlePanel("Explore"),
                           sidebarPanel(
                             selectInput("player_name", "Player Name", merged %>% pull(name) %>% sort()),
-                            h3("Common Pitches"),
+                            h5("Common Pitches"),
                             tags$ul(
                               tags$li("FF: Four-Seam Fastball"),
                               tags$li("FT: Two-Seam Fastball"),
@@ -116,6 +128,8 @@ ui <- navbarPage("Final Project",
                             plotOutput("seq_distr_plot")
                           )),
                  
+                 # describes the model used and goes through some data
+                 
                  tabPanel("Model",
                           titlePanel("Model"),
                           mainPanel(
@@ -130,7 +144,7 @@ ui <- navbarPage("Final Project",
                                      (i.e., difference in speed)"),
                               tags$li("another with the same information as the first, but also incorporating
                                       quantitative information about the pitch itself (i.e., absolute speed in
-                                      addition to relative speed"),
+                                      addition to relative speed)"),
                               tags$li("a model with all the information of the previous two, but now also 
                                       containing contextual information: the score, inning, number of outs,
                                       count, etc.")
@@ -179,22 +193,7 @@ ui <- navbarPage("Final Project",
                  tabPanel("About",
                           titlePanel("About"),
                           mainPanel(
-                            p("There's more data in baseball than ever now. Whether you're interested 
-                              in the probability a Mike Trout line drive will fall for a hit, the top 
-                              speed Ronald Acuña Jr. reaches going from first to third, or the precise 
-                              arm slot at which Josh Hader releases each fastball, these measurements 
-                              are all available at quite literally the push of a button. Yet as the 
-                              game becomes viewed by many as a series of sequences and estimations, 
-                              there remain questions about what hasn't yet been quantified."
-                            ),
-                            p("Some of it is, of course, not truly measurable - one can't reduce a 
-                              great teammate's impact on the clubhouse to fractions of a win or assess 
-                              how many points of stolen base percentage a savvy first-base coach might 
-                              add - but there's still room to improve analysis in certain areas. One of
-                              these is pitch sequencing: how to optimize variation in pitch type, speed,
-                              and location."
-                            ),
-                            p("In order to be able to properly analyze the effects of pitch sequencing,
+                            p("For this pitch sequencing analysis to be successful,
                               pitch-level MLB data is of course required. Thankfully, when MLB 
                               introduced Statcast, its player-tracking camera system, in 2015, they 
                               made all the data publicly available and accessible via ",
@@ -206,26 +205,20 @@ ui <- navbarPage("Final Project",
                                 href = "http://billpetti.github.io/baseballr/", 
                                 target = "_blank"),
                               "The past two years of data have been found to be more accurate than 
-                              2015-2017, but because we're primarily interested in basic pitch data and 
-                              less so more advanced metrics like spin rate and exit velocity, I feel 
+                              2015-2017, but because we're primarily interested in basic pitch data, I feel 
                               comfortable using all available Statcast measurements. In order to 
                               properly handle the multiple gigabytes worth of data, I used Git LFS and 
                               a modified form of Petti's", code("scrape_statcast_savant"), "function in
                               order to get the full five regular seasons of data."
                             ),
-                            p("As of right now, I believe this will be the only data source required 
-                              for this project, but should there be a material change I would consider 
-                              using", 
-                              a("Retrosheet play-by-play logs,", 
-                                href = "https://www.retrosheet.org", 
-                                target = "_blank"), "Sean Lahman's", 
-                              a("baseball database, ", 
-                                href = "http://www.seanlahman.com/baseball-archive/statistics/",
+                            p("In order to get player names for both pitchers and hitters, I had to use an
+                              external (non-MLB) site to get MLB player ID numbers, which I found at the 
+                              delightfully named ", 
+                              a("CrunchTimeBaseball", 
+                                href = "http://crunchtimebaseball.com/baseball_map.html", 
                                 target = "_blank"), 
-                              "or the very detailed", 
-                              a(code("openWAR"), "package.", 
-                                href = "https://github.com/beanumber/openWAR",
-                                target = "_blank")
+                              ". There were a handful of players missing from this source, but they were easy
+                              enough to manually look up on the Baseball Savant website."
                             ),
                             h3("Personal"),
                             p("My name is Tate Huffman, and I'm a junior at Harvard College studying 
@@ -245,14 +238,25 @@ ui <- navbarPage("Final Project",
 
 server <- function(input, output) {
   
+  # this code produces the plot seen in the "Explore" page of the Shiny app
+  
   output$seq_distr_plot <- renderPlot({
+    
+    # creates variable for number of sequences thrown/seen by selected player
+    # used later to calculate percent
+    
     numseq <- pitches_app %>% 
       filter(!!sym(paste0((merged %>% filter(name == input$player_name) %>% select(player_type) %>% pull()), 
                           "_name")) == input$player_name, 
              !is.na(pitch_seq)) %>% 
       nrow()
     
+    # title for plot
+    
     title_text <- paste0(input$playername, " Pitch Sequences")
+    
+    # gets sequences with seven highest frequencies
+    # standard ggplot() stuff
     
     pitches_app %>% 
       filter(!!sym(paste0((merged %>% filter(name == input$player_name) %>% select(player_type) %>% pull()), 
@@ -272,8 +276,11 @@ server <- function(input, output) {
       theme(legend.position = "none")
   })
   
+  # the following code produces the tables in the Shiny app's "Model" section
+  # for the prediction tables, we randomly get ten rows of data each time we run the app
+  
   output$preds_standard <- renderTable({
-    sample_n(predictions_standard, 8)
+    sample_n(predictions_standard, 10)
   })
   
   output$acc_standard <- renderTable({
@@ -281,7 +288,7 @@ server <- function(input, output) {
   })
   
   output$preds_simplified <- renderTable({
-    sample_n(predictions_simplified, 8)
+    sample_n(predictions_simplified, 10)
   })
   
   output$acc_simplified <- renderTable({
